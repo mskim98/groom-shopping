1aaf545030471ae48c381b9308be229c5d3b2127
kmgyu <lidersy961@gmail.com>
2025-10-31 14:22:24 +0900

    feat: 쿠폰 엔티티 구체화

--- backend/src/main/java/groom/backend/domain/coupon/entity/Coupon.java
+++ backend/src/main/java/groom/backend/domain/coupon/entity/Coupon.java
@@ -1,5 +1,39 @@
 package groom.backend.domain.coupon.entity;
 
+import groom.backend.domain.coupon.enums.CouponType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Coupon {
+  @Id
+  private Long id;
+
+  private String name;
+
+  private String description;
+
+  private Long quantity;
+
+  private Integer amount;
+
+  private Boolean isActive;
+
+  @Enumerated(EnumType.STRING)
+  private CouponType type;
+
+  private LocalDate expireDate;
 
 }
--- backend/src/main/java/groom/backend/domain/coupon/entity/CouponIssue.java
+++ backend/src/main/java/groom/backend/domain/coupon/entity/CouponIssue.java
@@ -1,4 +1,43 @@
 package groom.backend.domain.coupon.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class CouponIssue {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private Boolean isActive;
+
+  private LocalDateTime createdAt;
+
+  private LocalDateTime updatedAt;
+
+
+  // coupon 1 : coupon issue N
+//  @ManyToOne(fetch = FetchType.LAZY)
+//  @JoinColumn(name = "user_id", nullable = false)
+//  private User user;
+  private Long userId;
+
+
+  // coupon 1 : coupon issue N
+//  @ManyToOne(fetch = FetchType.LAZY)
+//  @JoinColumn(name = "coupon_id", nullable = false)
+//  private Coupon coupon;
+  private Long couponId;
 }
