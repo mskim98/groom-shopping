1060716f8c372b7042a1edb3c3103184052a1fb3
JunwonLee <jununida@gmail.com>
2025-10-31 19:57:53 +0900

    feat: 회원가입 Entity 생성
    - User entity생성
    - entity에서 사용할 등급, role enums 생성

---
+++ backend/src/main/java/groom/backend/domain/auth/entity/User.java
@@ -0,0 +1,47 @@
+package groom.backend.domain.auth.entity;
+
+
+import groom.backend.domain.auth.enums.Grade;
+import groom.backend.domain.auth.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 50)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Grade grade;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
