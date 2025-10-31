ba63dfa27ea273102dcf0cfed2ce902bca03c6fa
kmgyu <lidersy961@gmail.com>
2025-10-31 21:37:07 +0900

    feat: 쿠폰 발급 반환 dto 정의

---
+++ backend/src/main/java/groom/backend/domain/coupon/dto/response/IssueCouponResponse.java
@@ -0,0 +1,19 @@
+package groom.backend.domain.coupon.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class IssueCouponResponse {
+  private Long couponIssueId;
+  private Long couponId;
+  private LocalDate createdAt;
+  private LocalDate deletedAt;
+}
