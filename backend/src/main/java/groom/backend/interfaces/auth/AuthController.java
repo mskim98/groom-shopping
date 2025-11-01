fc08498c6b529f59e46d88a249f120d922ef78ea
JunwonLee <jununida@gmail.com>
2025-11-01 16:05:00 +0900

    fix: 요청 파라미터에 @Valid 어노테이션 추가

--- backend/src/main/java/groom/backend/interfaces/auth/AuthController.java
+++ backend/src/main/java/groom/backend/interfaces/auth/AuthController.java
@@ -3,6 +3,7 @@ package groom.backend.interfaces.auth;
 import groom.backend.application.auth.service.AuthApplicationService;
 import groom.backend.interfaces.auth.dto.request.SignUpRequest;
 import groom.backend.interfaces.auth.dto.response.SignUpResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ public class AuthController {
     private final AuthApplicationService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<SignUpResponse> signUp(@RequestBody SignUpRequest request) {
+    public ResponseEntity<SignUpResponse> signUp(@RequestBody @Valid SignUpRequest request) {
         SignUpResponse response = authService.register(request);
         return ResponseEntity.ok(response);
     }
