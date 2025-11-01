124b898d97b7e9d5853d27d515444e4c7c707f3f
JunwonLee <jununida@gmail.com>
2025-11-01 19:14:24 +0900

    feat: 로그인 인증 관련 환경설정 추가

--- backend/src/main/java/groom/backend/infrastructure/config/SecurityConfig.java
+++ backend/src/main/java/groom/backend/infrastructure/config/SecurityConfig.java
@@ -1,14 +1,23 @@
 package groom.backend.infrastructure.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -18,13 +27,27 @@ public class SecurityConfig {
                         .anyRequest().permitAll() // 모든 요청 허용
                 )
                 .formLogin(login -> login.disable()) // 로그인 폼 비활성화
-                .httpBasic(basic -> basic.disable()); // 기본 인증 비활성화
+                .httpBasic(basic -> basic.disable()) // 기본 인증 비활성화
+                .authenticationProvider(authenticationProvider()); // 커스텀 AuthenticationProvider 등록
 
         return http.build();
     }
 
     @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
