901a9132c10e9b41886f3c0597cffd0100fe91c2
JunwonLee <jununida@gmail.com>
2025-11-01 13:24:23 +0900

    feat: 회원가입 controller 구현
    end point : /auth/signup
    parameter : email, password, name
    권한 : default USER
    등급 : default BRONZE

--- backend/src/main/java/groom/backend/interfaces/auth/AuthController.java
+++ backend/src/main/java/groom/backend/interfaces/auth/AuthController.java
@@ -1,4 +1,24 @@
 package groom.backend.interfaces.auth;
 
+import groom.backend.application.auth.service.AuthApplicationService;
+import groom.backend.interfaces.auth.dto.request.SignUpRequest;
+import groom.backend.interfaces.auth.dto.response.SignUpResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthApplicationService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<SignUpResponse> signUp(@RequestBody SignUpRequest request) {
+        SignUpResponse response = authService.register(request);
+        return ResponseEntity.ok(response);
+    }
 }
