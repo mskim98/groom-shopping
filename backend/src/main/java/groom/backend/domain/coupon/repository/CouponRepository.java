67616cba4e44bc764afdc3d9034c2209a8adca8d
kmgyu <lidersy961@gmail.com>
2025-11-03 00:00:40 +0900

    feat: 쿠폰 리포지토리 쿠폰 조건 검색 기능 구현 - JPQL을 통한 구현 구체화

--- backend/src/main/java/groom/backend/domain/coupon/repository/CouponRepository.java
+++ backend/src/main/java/groom/backend/domain/coupon/repository/CouponRepository.java
@@ -2,12 +2,29 @@ package groom.backend.domain.coupon.repository;
 
 import groom.backend.interfaces.coupon.dto.request.CouponSearchCondition;
 import groom.backend.domain.coupon.entity.Coupon;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
-  Page<Coupon> searchByCondition(CouponSearchCondition condition, Pageable pageable);
+  @Query("""
+  SELECT c
+  FROM Coupon c
+  WHERE
+    (:#{#cond.name} IS NULL OR c.name LIKE %:#{#cond.name}%)
+    AND (:#{#cond.description} IS NULL OR c.description LIKE %:#{#cond.description}%)
+    AND (:#{#cond.type} IS NULL OR c.type = :#{#cond.type})
+    AND (:#{#cond.isActive} IS NULL OR c.isActive = :#{#cond.isActive})
+    AND (:#{#cond.expireDateFrom} IS NULL OR c.expireDate >= :#{#cond.expireDateFrom})
+    AND (:#{#cond.expireDateTo} IS NULL OR c.expireDate <= :#{#cond.expireDateTo})
+    AND (:#{#cond.minQuantity} IS NULL OR c.quantity >= :#{#cond.minQuantity})
+    AND (:#{#cond.maxQuantity} IS NULL OR c.quantity <= :#{#cond.maxQuantity})
+    AND (:#{#cond.minAmount} IS NULL OR c.amount >= :#{#cond.minAmount})
+    AND (:#{#cond.maxAmount} IS NULL OR c.amount <= :#{#cond.maxAmount})
+  """)
+  Page<Coupon> searchByCondition(@Param("condition") CouponSearchCondition condition, Pageable pageable);
 }
