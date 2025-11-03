d22c2b39b42c52e1dc35d763688a70fa233505ab
JunwonLee <jununida@gmail.com>
2025-11-03 16:52:36 +0900

    feat : 추첨 마스터 CRUD를 위한 repository 추가

--- backend/src/main/java/groom/backend/domain/raffle/repository/RaffleRepository.java
+++ backend/src/main/java/groom/backend/domain/raffle/repository/RaffleRepository.java
@@ -1,4 +1,17 @@
 package groom.backend.domain.raffle.repository;
 
+import groom.backend.domain.raffle.entity.Raffle;
+import groom.backend.interfaces.raffle.dto.request.RaffleSearchRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
 public interface RaffleRepository {
+    Optional<Raffle> findById(Long id);
+    Raffle save(Raffle raffle);
+    boolean existsByRaffleProductId(String raffleProductId);
+    void deleteById(Long id);
+
+    Page<Raffle> search(RaffleSearchRequest cond, Pageable pageable);
 }
