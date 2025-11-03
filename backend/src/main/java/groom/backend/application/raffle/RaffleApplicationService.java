262ef112c002e7527bd85ef27c6c0a05e633e4b4
JunwonLee <jununida@gmail.com>
2025-11-03 16:53:48 +0900

    feat : 추첨 마스터정보의 CRUD를 위한 service 구현

--- backend/src/main/java/groom/backend/application/raffle/RaffleApplicationService.java
+++ backend/src/main/java/groom/backend/application/raffle/RaffleApplicationService.java
@@ -1,4 +1,137 @@
 package groom.backend.application.raffle;
 
+import groom.backend.domain.auth.entity.User;
+import groom.backend.domain.auth.enums.Role;
+import groom.backend.domain.raffle.entity.Raffle;
+import groom.backend.domain.raffle.enums.RaffleStatus;
+import groom.backend.domain.raffle.repository.RaffleRepository;
+import groom.backend.interfaces.raffle.dto.request.RaffleRequest;
+import groom.backend.interfaces.raffle.dto.request.RaffleSearchRequest;
+import groom.backend.interfaces.raffle.dto.response.RaffleResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class RaffleApplicationService {
+
+    private final RaffleRepository raffleRepository;
+
+    public Page<RaffleResponse> searchRaffles(User user, RaffleSearchRequest cond, Pageable pageable) {
+        Page<Raffle> page = raffleRepository.search(cond, pageable);
+        return page.map(RaffleResponse::from);
+    }
+
+    public RaffleResponse getRaffleDetails(User user, Long raffleId) {
+        Raffle raffle = raffleRepository.findById(raffleId)
+                .orElseThrow(() -> new IllegalStateException("해당 ID의 추첨이 존재하지 않습니다."));
+        return RaffleResponse.from(raffle);
+    }
+
+    @Transactional
+    public RaffleResponse createRaffle(User user, RaffleRequest request) {
+
+        if(isNotAdmin(user)) {
+            throw new IllegalStateException("관리자만 추첨을 생성할 수 있습니다.");
+        }
+
+        validateDateRaffleRequest(request);
+
+        if(raffleRepository.existsByRaffleProductId(request.getRaffleProductId())) {
+            throw new IllegalStateException("해당 상품으로 등록된 추첨이 이미 존재합니다.");
+        }
+
+        Raffle raffle = new Raffle(
+                null,
+                request.getRaffleProductId(),
+                request.getWinnerProductId(),
+                request.getTitle(),
+                request.getDescription(),
+                request.getWinnersCount(),
+                request.getMaxEntriesPerUser(),
+                request.getEntryStartAt(),
+                request.getEntryEndAt(),
+                request.getRaffleDrawAt(),
+                RaffleStatus.DRAFT,
+                null,
+                null
+        );
+
+        Raffle saved = raffleRepository.save(raffle);
+
+        return RaffleResponse.from(saved);
+    }
+
+    @Transactional
+    public RaffleResponse updateRaffle(User user, Long raffleId, RaffleRequest request) {
+
+        if(isNotAdmin(user)) {
+            throw new IllegalStateException("관리자만 추첨을 수정할 수 있습니다.");
+        }
+
+        validateDateRaffleRequest(request);
+
+        Raffle raffle = raffleRepository.findById(raffleId)
+                .orElseThrow(() -> new IllegalStateException("해당 ID의 추첨이 존재하지 않습니다."));
+
+        if(raffle.getStatus() != RaffleStatus.DRAFT) {
+            throw new IllegalStateException("진행중이거나 종료된 추첨은 수정할 수 없습니다.");
+        }
+
+        if(!raffle.getRaffleProductId().equals(request.getRaffleProductId())) {
+            if(raffleRepository.existsByRaffleProductId(request.getRaffleProductId())) {
+                throw new IllegalStateException("해당 상품으로 등록된 추첨이 이미 존재합니다.");
+            }
+        }
+
+        raffle.updateRaffle(request);
+
+        Raffle saved = raffleRepository.save(raffle);
+
+        return RaffleResponse.from(saved);
+    }
+
+    @Transactional
+    public void deleteRaffle(User user, Long raffleId) {
+
+        if(isNotAdmin(user)) {
+            throw new IllegalStateException("관리자만 추첨을 삭제할 수 있습니다.");
+        }
+
+        Raffle raffle = raffleRepository.findById(raffleId)
+                .orElseThrow(() -> new IllegalStateException("해당 ID의 추첨이 존재하지 않습니다."));
+
+        if(raffle.getStatus() != RaffleStatus.DRAFT) {
+            throw new IllegalStateException("진행중이거나 종료된 추첨은 삭제할 수 없습니다.");
+        }
+
+        raffleRepository.deleteById(raffle.getRaffleId());
+    }
+
+
+
+    private boolean isNotAdmin(User user) {
+        return user.getRole() != Role.ROLE_ADMIN;
+    }
+
+    private void validateDateRaffleRequest(RaffleRequest request) {
+        // TODO : request 검증 로직 추가 필요
+        // RaffleProductId, WinnerProductId 존재하는지 등
+
+
+        if(request.getEntryEndAt().isBefore(request.getEntryStartAt())) {
+            throw new IllegalStateException("응모 종료일은 응모 시작일 이후여야 합니다.");
+        }
+
+        if(request.getRaffleDrawAt().isBefore(request.getEntryEndAt())) {
+            throw new IllegalStateException("추첨일은 응모 종료일 이후여야 합니다.");
+        }
+    }
+
 }
