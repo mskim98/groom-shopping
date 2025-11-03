2c5133efc956db718daa68ec8debca1b1a09a6e5
kmgyu <lidersy961@gmail.com>
2025-11-03 10:57:37 +0900

    fix: JPQL 잘못 작성된 SpEL 수정 - condition 축악여로 cond 작성 후 parameter 수정하지 않은 문제 수정. condition으로 통일

--- backend/src/main/java/groom/backend/domain/coupon/repository/CouponRepository.java
+++ backend/src/main/java/groom/backend/domain/coupon/repository/CouponRepository.java
@@ -15,16 +15,16 @@ public interface CouponRepository extends JpaRepository<Coupon, Long> {
   SELECT c
   FROM Coupon c
   WHERE
-    (:#{#cond.name} IS NULL OR c.name LIKE %:#{#cond.name}%)
-    AND (:#{#cond.description} IS NULL OR c.description LIKE %:#{#cond.description}%)
-    AND (:#{#cond.type} IS NULL OR c.type = :#{#cond.type})
-    AND (:#{#cond.isActive} IS NULL OR c.isActive = :#{#cond.isActive})
-    AND (:#{#cond.expireDateFrom} IS NULL OR c.expireDate >= :#{#cond.expireDateFrom})
-    AND (:#{#cond.expireDateTo} IS NULL OR c.expireDate <= :#{#cond.expireDateTo})
-    AND (:#{#cond.minQuantity} IS NULL OR c.quantity >= :#{#cond.minQuantity})
-    AND (:#{#cond.maxQuantity} IS NULL OR c.quantity <= :#{#cond.maxQuantity})
-    AND (:#{#cond.minAmount} IS NULL OR c.amount >= :#{#cond.minAmount})
-    AND (:#{#cond.maxAmount} IS NULL OR c.amount <= :#{#cond.maxAmount})
+    (:#{#condition.name} IS NULL OR c.name LIKE %:#{#condition.name}%)
+    AND (:#{#condition.description} IS NULL OR c.description LIKE %:#{#condition.description}%)
+    AND (:#{#condition.type} IS NULL OR c.type = :#{#condition.type})
+    AND (:#{#condition.isActive} IS NULL OR c.isActive = :#{#condition.isActive})
+    AND (:#{#condition.expireDateFrom} IS NULL OR c.expireDate >= :#{#condition.expireDateFrom})
+    AND (:#{#condition.expireDateTo} IS NULL OR c.expireDate <= :#{#condition.expireDateTo})
+    AND (:#{#condition.minQuantity} IS NULL OR c.quantity >= :#{#condition.minQuantity})
+    AND (:#{#condition.maxQuantity} IS NULL OR c.quantity <= :#{#condition.maxQuantity})
+    AND (:#{#condition.minAmount} IS NULL OR c.amount >= :#{#condition.minAmount})
+    AND (:#{#condition.maxAmount} IS NULL OR c.amount <= :#{#condition.maxAmount})
   """)
   Page<Coupon> searchByCondition(@Param("condition") CouponSearchCondition condition, Pageable pageable);
 }
