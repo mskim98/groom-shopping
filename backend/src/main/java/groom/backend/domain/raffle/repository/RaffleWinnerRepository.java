3b9a5f8d1368f9aa8a8ddde7381703b58a7cb0a2
JunwonLee <jununida@gmail.com>
2025-11-04 20:46:30 +0900

    feat : Repository 생성
    pickWinnersNative -> 추첨 실행
    findNotificationsByRaffleId -> 래플아이디로 당첨자에게 메세지 보낼때 필요한 데이터 추출
    countByRaffleTicket_Raffle_RaffleId -> 현재 래플이 당첨자 수 추출 -> 재실행될때 총 당첨자 수 - 현재 당첨자 수

--- backend/src/main/java/groom/backend/domain/raffle/repository/RaffleWinnerRepository.java
+++ backend/src/main/java/groom/backend/domain/raffle/repository/RaffleWinnerRepository.java
@@ -1,4 +1,16 @@
 package groom.backend.domain.raffle.repository;
 
+import groom.backend.domain.raffle.entity.RaffleWinner;
+import groom.backend.interfaces.raffle.dto.notification.RaffleWinnerNotification;
+import groom.backend.interfaces.raffle.dto.request.RaffleDrawCondition;
+
+import java.util.List;
+
 public interface RaffleWinnerRepository {
+    RaffleWinner save(RaffleWinner raffleWinner);
+
+    List<RaffleWinnerNotification> findNotificationsByRaffleId(Long raffleId);
+
+    int countWinnerByRaffleId(Long raffleId);
+    int pickWinnersNative (RaffleDrawCondition condition);
 }
