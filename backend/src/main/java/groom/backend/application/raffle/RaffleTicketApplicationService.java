ea2044632e5199d28e6af3f3c3d0d50caabb858f
JunwonLee <jununida@gmail.com>
2025-11-05 23:46:07 +0900

    feat : 응모 검증 후 장바구니추가
    장바구니 추가 로직
    기존에 추첨 상품 검증 중복이어서 제거

--- backend/src/main/java/groom/backend/application/raffle/RaffleTicketApplicationService.java
+++ backend/src/main/java/groom/backend/application/raffle/RaffleTicketApplicationService.java
@@ -1,9 +1,8 @@
 package groom.backend.application.raffle;
 
-import groom.backend.domain.raffle.criteria.RaffleValidationCriteria;
+import groom.backend.application.cart.CartApplicationService;
 import groom.backend.domain.raffle.entity.Raffle;
 import groom.backend.domain.raffle.entity.RaffleTicket;
-import groom.backend.domain.raffle.repository.RaffleRepository;
 import groom.backend.domain.raffle.repository.RaffleTicketRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,25 +13,30 @@ import org.springframework.transaction.annotation.Transactional;
 public class RaffleTicketApplicationService {
 
     private final RaffleTicketAllocationService allocationService;
-    private final RaffleRepository raffleRepository;
     private final RaffleTicketRepository raffleTicketRepo;
     private final RaffleValidationService validationService;
+    private final CartApplicationService cartApplicationService;;
 
     // 응모 장바구니에 저장
     public void addToEntryCart(Long raffleId, Long userId, int count) {
         Raffle raffle = validationService.findById(raffleId);
-        // 상품 존재 및 상태 ,재고 검증
+        // 상품 존재 및 상태 ,재고 검증 (add To Cart 내부에서 처리함)
+        /*
         RaffleValidationCriteria criteria = RaffleValidationCriteria.builder()
                 .raffleProductId(raffle.getRaffleProductId())
                 .build();
         validationService.validateProductsForRaffle(criteria);
+        */
 
         // 응모 가능 여부 검증 (응모 기간, 상태)
         validationService.validateRaffleForEntry(raffle);
 
         // 사용자 응모 한도 검증
         validationService.validateUserEntryLimit(raffle, userId, count);
-        // TODO 장바구니 로직 추가 - 실제 티켓은 결제 완료 후 생성
+
+        // 장바구니에 응모 상품 추가
+        // TODO : 예외 발생 시 처리 방식 확인 필요
+        cartApplicationService.addToCart(userId, raffle.getRaffleProductId(), count);
 
     }
 
