75211c4cdefe6365e7c910585376146a7e54c011
kmgyu <lidersy961@gmail.com>
2025-11-10 16:52:00 +0900

    docs: 전략 패턴 메서드 주석 추가

--- backend/src/main/java/groom/backend/domain/coupon/policy/DiscountStrategy.java
+++ backend/src/main/java/groom/backend/domain/coupon/policy/DiscountStrategy.java
@@ -2,6 +2,12 @@ package groom.backend.domain.coupon.policy;
 
 // 전략 패턴 인터페이스
 public interface DiscountStrategy {
+  /**
+   * 할인 금액을 계산합니다.
+   * @param cost 총 구매 금액
+   * @param amount 정책에 사용되는 할인 매개변수
+   * @return
+   */
   int calculateDiscount(int cost, int amount);
 }
 
