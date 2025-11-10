65dd357cd69146137fa344f0a1d4f16dee89c286
kmgyu <lidersy961@gmail.com>
2025-11-10 23:55:18 +0900

    feat: 신규 정책 enum 및 팩토리 패턴에 적용 - 최소 구매 금액 정책 - 최대 할인 금액 정책 - 다중 쿠폰 사용 전략용 메서드 추가

--- backend/src/main/java/groom/backend/domain/coupon/policy/DiscountStrategyFactory.java
+++ backend/src/main/java/groom/backend/domain/coupon/policy/DiscountStrategyFactory.java
@@ -7,6 +7,16 @@ public class DiscountStrategyFactory {
     return switch (couponType) {
       case DISCOUNT -> new DiscountAmountMultiStrategy();
       case PERCENT -> new DiscountPercentMultiStrategy();
+      case MIN_COST_AMOUNT -> new DiscountAmountMultiStrategy();
+      case MAX_DISCOUNT_PERCENT -> new DiscountAmountMultiStrategy();
+    };
+  }
+
+  public static DiscountMultiStrategy getDiscountMultiStrategy(CouponType couponType) {
+    return switch (couponType) {
+      case DISCOUNT -> new DiscountAmountMultiStrategy();
+      case PERCENT -> new DiscountPercentMultiStrategy();
+      default -> null;
     };
   }
 }
