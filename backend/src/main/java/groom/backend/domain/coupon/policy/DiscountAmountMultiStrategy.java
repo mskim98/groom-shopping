af09192ce1929436759dd87a4cd0ed7463ac10a5
kmgyu <lidersy961@gmail.com>
2025-11-10 17:55:25 +0900

    refactor: 전략 패턴에서 DiscountContext를 사용하도록 리팩토링

--- backend/src/main/java/groom/backend/domain/coupon/policy/DiscountAmountMultiStrategy.java
+++ backend/src/main/java/groom/backend/domain/coupon/policy/DiscountAmountMultiStrategy.java
@@ -1,13 +1,14 @@
 package groom.backend.domain.coupon.policy;
 
+import groom.backend.domain.coupon.model.vo.DiscountContext;
 import org.springframework.stereotype.Component;
 
 // 정액 할인 정책
 @Component
 public class DiscountAmountMultiStrategy implements DiscountMultiStrategy {
   @Override
-  public int calculateDiscount(int cost, int amount) {
-    return amount; // amount 만큼 차감
+  public int calculateDiscount(DiscountContext discountContext) {
+    return discountContext.getAmount(); // amount 만큼 차감
   }
 }
 
--- backend/src/main/java/groom/backend/domain/coupon/policy/DiscountPercentMultiStrategy.java
+++ backend/src/main/java/groom/backend/domain/coupon/policy/DiscountPercentMultiStrategy.java
@@ -1,13 +1,14 @@
 package groom.backend.domain.coupon.policy;
 
+import groom.backend.domain.coupon.model.vo.DiscountContext;
 import org.springframework.stereotype.Component;
 
 // 비율 할인 정책
 @Component
 public class DiscountPercentMultiStrategy implements DiscountMultiStrategy {
   @Override
-  public int calculateDiscount(int cost, int amount) {
-    double rate = amount / 100.0;
-    return (int) Math.floor((cost * rate) / 100) * 100; // 백원 단위 절삭
+  public int calculateDiscount(DiscountContext discountContext) {
+    double rate = discountContext.getAmount() / 100.0;
+    return (int) Math.floor((discountContext.getCost() * rate) / 100) * 100; // 백원 단위 절삭
   }
 }
