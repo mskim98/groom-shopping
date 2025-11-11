b1df49d85b6346d37a5c21883d9b4ae0235813f3
kmgyu <lidersy961@gmail.com>
2025-11-11 14:20:33 +0900

    fix: ProductSearchCondition Criteria 필드 타입 변경 - vo로 인한 이슈 발생 가능, 필드 타입 변경

--- backend/src/main/java/groom/backend/domain/product/model/criteria/ProductSearchCondition.java
+++ backend/src/main/java/groom/backend/domain/product/model/criteria/ProductSearchCondition.java
@@ -2,18 +2,24 @@ package groom.backend.domain.product.model.criteria;
 
 import groom.backend.domain.product.model.enums.ProductCategory;
 import groom.backend.domain.product.model.enums.ProductStatus;
-import groom.backend.domain.product.model.vo.Price;
+import lombok.*;
 import org.springframework.data.domain.Sort;
 
 /**
  * 상품 검색/정렬/필터링 정보를 제공하는 criteria
  */
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProductSearchCondition {
   // 검색 조건
   private String name;
 
-  private Price minPrice;
-  private Price maxPrice;
+  // Price vo가 유효성 검증하기 때문에 null이 들어가면 안됨.
+  // Argument Exception 발생가능하여 타입 수정
+  private Integer minPrice;
+  private Integer maxPrice;
 
   // 필터링 조건
   private ProductStatus status; // 제품 판매 삳태
