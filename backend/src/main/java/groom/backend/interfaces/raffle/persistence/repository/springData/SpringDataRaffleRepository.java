3b4635fa875d31a9ac9ba3b05bf22f2ab4ebeaeb
JunwonLee <jununida@gmail.com>
2025-11-14 17:35:21 +0900

    feat : 상태변경 대상 추출 repository 추구

--- backend/src/main/java/groom/backend/interfaces/raffle/persistence/repository/springData/SpringDataRaffleRepository.java
+++ backend/src/main/java/groom/backend/interfaces/raffle/persistence/repository/springData/SpringDataRaffleRepository.java
@@ -1,10 +1,14 @@
 package groom.backend.interfaces.raffle.persistence.repository.springData;
 
 import groom.backend.domain.raffle.entity.Raffle;
+import groom.backend.domain.raffle.enums.RaffleStatus;
 import groom.backend.interfaces.raffle.persistence.Entity.RaffleJpaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,4 +16,16 @@ public interface SpringDataRaffleRepository extends JpaRepository<RaffleJpaEntit
     boolean existsByRaffleProductId(UUID raffleProductId);
 
     Optional<Raffle> findByRaffleProductId(UUID raffleProductId);
+
+    // Ready -> Active 전환용
+    Page<RaffleJpaEntity> findAllByStatusAndEntryStartAtBefore(RaffleStatus status, LocalDateTime now, Pageable pageable);
+
+    // Active -> CLOSED 전환용
+    Page<RaffleJpaEntity> findAllByStatusAndEntryEndAtBefore(RaffleStatus status, LocalDateTime now, Pageable pageable);
+
+    // Draw 진행용
+    Page<RaffleJpaEntity> findAllByStatusAndRaffleDrawAtBefore(RaffleStatus status, LocalDateTime now, Pageable pageable);
+
+
+
 }
