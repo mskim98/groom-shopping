fe6719bb3160d73b4021ca78f6cfb5d1f614192d
kmgyu <lidersy961@gmail.com>
2025-11-14 14:03:57 +0900

    fix: CouponType 수정에 따른 인식 불가 현상 수정

--- backend/src/main/java/groom/backend/domain/coupon/model/enums/CouponType.java
+++ backend/src/main/java/groom/backend/domain/coupon/model/enums/CouponType.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum CouponType {
   @JsonProperty("PERCENT")  PERCENT,
-  @JsonProperty("DISCOUNT")  DISCOUNT,
+  @JsonProperty("AMOUNT")  DISCOUNT,
   @JsonProperty("MIN_COST_AMOUNT") MIN_COST_AMOUNT,
   @JsonProperty("MAX_DISCOUNT_PERCENT") MAX_DISCOUNT_PERCENT
 }
