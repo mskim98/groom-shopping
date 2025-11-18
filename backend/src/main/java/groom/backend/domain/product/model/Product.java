93f17cca797840c31788e5e6f2c1a8ce265f4a26
kmgyu <lidersy961@gmail.com>
2025-11-18 10:02:46 +0900

    fix: Product 엔티티, dto, repository에 imageUrl 추가

--- backend/src/main/java/groom/backend/domain/product/model/Product.java
+++ backend/src/main/java/groom/backend/domain/product/model/Product.java
@@ -37,7 +37,8 @@ public class Product {
             Stock stock,
             ProductCategory category,
             Integer thresholdValue,
-            Boolean isActive) {
+            Boolean isActive,
+            String imageUrl) {
         Product product = new Product();
         product.id = id;
         product.name = name;
@@ -50,6 +51,7 @@ public class Product {
         product.status = stock.isEmpty()
                 ? ProductStatus.OUT_OF_STOCK
                 : ProductStatus.AVAILABLE;
+        product.imageUrl = imageUrl;
         return product;
     }
 
